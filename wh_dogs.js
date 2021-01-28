7da94c135fcd40fc2f101adb5863325515517f73
Amy Guo <72362221+amyeguo@users.noreply.github.com>
2021-01-28 15:51:35 -0600

    Add files via upload

---
+++ wh_dogs.js
@@ -0,0 +1,140 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// left side
+gsap.to("#content1", {
+  scrollTrigger: {
+    trigger: "#content1",
+    toggleActions: "resume pause resume pause",
+  },
+  xPercent: 67,
+  duration: 1.4,
+});
+
+// right side
+gsap.to("#content2", {
+  scrollTrigger: {
+    trigger: "#content2",
+  },
+  xPercent: -167,
+  duration: 1.4,
+});
+
+// left side
+gsap.to("#content3", {
+  scrollTrigger: {
+    trigger: "#content3",
+    toggleActions: "resume pause resume pause",
+  },
+  xPercent: 67,
+  duration: 1.4,
+});
+
+// right side
+gsap.to("#content4", {
+  scrollTrigger: {
+    trigger: "#content4",
+    toggleActions: "resume pause resume pause",
+  },
+  xPercent: -167,
+  duration: 1.4,
+});
+
+// left side
+gsap.to("#content5", {
+  scrollTrigger: {
+    trigger: "#content5",
+    toggleActions: "resume pause resume pause",
+  },
+  xPercent: 67,
+  duration: 1.4,
+});
+
+// right side
+gsap.to("#content6", {
+  scrollTrigger: {
+    trigger: "#content6",
+    toggleActions: "resume pause resume pause",
+  },
+  xPercent: -167,
+  duration: 1.4,
+});
+
+// left side
+gsap.to("#content7", {
+  scrollTrigger: {
+    trigger: "#content7",
+    toggleActions: "resume pause resume pause",
+  },
+  xPercent: 67,
+  duration: 1.4,
+});
+
+// right side
+gsap.to("#content8", {
+  scrollTrigger: {
+    trigger: "#content8",
+    toggleActions: "resume pause resume pause",
+  },
+  xPercent: -167,
+  duration: 1.4,
+});
+
+// left side
+gsap.to("#content9", {
+  scrollTrigger: {
+    trigger: "#content9",
+    toggleActions: "resume pause resume pause",
+  },
+  xPercent: 67,
+  duration: 1.4,
+});
+
+// right side
+gsap.to("#content10", {
+  scrollTrigger: {
+    trigger: "#content10",
+    toggleActions: "resume pause resume pause",
+  },
+  xPercent: -167,
+  duration: 1.4,
+});
+
+// left side
+gsap.to("#content11", {
+  scrollTrigger: {
+    trigger: "#content11",
+    toggleActions: "resume pause resume pause",
+  },
+  xPercent: 66,
+  duration: 1.4,
+});
+
+// right side
+gsap.to("#content12", {
+  scrollTrigger: {
+    trigger: "#content12",
+    toggleActions: "resume pause resume pause",
+  },
+  xPercent: -167,
+  duration: 1.4,
+});
+
+// left side
+gsap.to("#content13", {
+  scrollTrigger: {
+    trigger: "#content13",
+    toggleActions: "resume pause resume pause",
+  },
+  xPercent: 66,
+  duration: 1.4,
+});
+
+// right side
+gsap.to("#content14", {
+  scrollTrigger: {
+    trigger: "#content14",
+    toggleActions: "resume pause resume pause",
+  },
+  xPercent: -167,
+  duration: 1.4,
+});
